16c0609f60cf1ead67e3799836154db1602aac8f
Manuel <mbm0035@alu.medac.es>
2025-11-09 03:27:36 +0100

    Ejercicio1
    Tocar de todo, pero se ha conseguido

--- app/src/main/java/com/example/practica_5_login/MainActivity.java
+++ app/src/main/java/com/example/practica_5_login/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.practica_5_login;
 
 import android.os.Bundle;
-
+import android.view.View;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText tCorreo;
+    private EditText tContraseña;
+    private Button bContinuar;
+    private Switch sRecordar;
+    private TextView textView5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +28,22 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        tCorreo = findViewById(R.id.edCorreo);
+        tContraseña = findViewById(R.id.edContraseña);
+        bContinuar = findViewById(R.id.Continuar);
+        sRecordar = findViewById(R.id.sRecordar);
+        textView5 = findViewById(R.id.textView5);
+
+        bContinuar.setOnClickListener(v ->  {
+            String correo = tCorreo.getText().toString();
+            String contraseña = tContraseña.getText().toString();
+
+            if (correo.equals("correo@gmail.com") && contraseña.equals("123")){
+                textView5.setText("usuario y contraseña correctos");
+            }else{
+                textView5.setText("usuario o contraseña incorrectos");
+            }
         });
     }
 }
