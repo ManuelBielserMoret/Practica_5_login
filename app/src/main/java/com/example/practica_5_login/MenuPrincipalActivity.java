fd9adb93100d536f668fe0374c9486493f0c4b09
Manuel <mbm0035@alu.medac.es>
2025-11-12 11:41:01 +0100

    parte 2 entero hecho y funciona

--- app/src/main/java/com/example/practica_5_login/MenuPrincipalActivity.java
+++ app/src/main/java/com/example/practica_5_login/MenuPrincipalActivity.java
@@ -1,24 +1,40 @@
 package com.example.practica_5_login;
 
 import android.os.Bundle;
-
+import android.content.Intent;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MenuPrincipalActivity extends AppCompatActivity {
+
+    private TextView textviewEmail;
+    private Button btnDesconectar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_menu_principal);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        textviewEmail = findViewById(R.id.viewEmail);
+        btnDesconectar = findViewById(R.id.botonDesconectar);
+
+        Intent intent = getIntent();
+        String emailRecibido = intent.getStringExtra("Email_Usuario");
+        if (emailRecibido != null) {
+            textviewEmail.setText(emailRecibido);
+        }
+        btnDesconectar.setOnClickListener(v -> {
+            finish();
+        });
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.viewEmail), (v, insets) -> {
+                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                return insets;
         });
     }
 }
